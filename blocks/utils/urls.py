5ace9c7372856e34917d96a1b72a858fd73f85ab
kimus <kimus.linuxus@gmail.com>
2013-10-23 11:32:23 +0100

    sync

--- blocks/utils/urls.py
+++ blocks/utils/urls.py
@@ -1,13 +1,14 @@
 from django.conf import settings
 import urlparse
 
-def get_menu_url(keyword):
+def get_menu_url(keyword, default=None):
 	from blocks.models import Menu
 	url = ''
 	try:
 		url = Menu.objects.get(keyword=keyword).url
 	except:
-		pass
+		if default:
+			url = '/' + default + '/'
 	return url
 
 def is_absolute_url(url):
